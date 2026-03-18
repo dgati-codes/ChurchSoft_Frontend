53c7c146ad0bfdb3c5329814b3fb3a66482e3c68
Geraldkpelevi <davigerald155@gmail.com>
2026-03-18 11:34:05 +0300

    birthday bug fixed

--- src/Layouts/Dashboard/Dashboard.jsx
+++ src/Layouts/Dashboard/Dashboard.jsx
@@ -43,7 +43,7 @@ export default function Dashboard() {
   const newMembers = newMembersData?.content || newMembersData || [];
   const birthdays = birthdaysData?.content || birthdaysData || [];
   // console.log(newMembers);
-
+  const hasBirthdays = birthdays.length > 0;
   const birthdayRef = useRef(null);
   const scrollLeft = () => {
     birthdayRef.current.scrollBy({
@@ -100,11 +100,13 @@ export default function Dashboard() {
 
         {/* BIRTHDAYS + EVENTS */}
         <div className="grid grid-cols-[1.3fr_1fr] gap-6 mb-4">
-          {/* BIRTHDAYS */}
-          {/* BIRTHDAYS */}
-          <div className=" bg-white rounded-2xl p-6">
-            
-
+          <div
+            className={`rounded-2xl p-6 border ${
+              hasBirthdays
+                ? "bg-white border-gray-100"
+                : "bg-orange-50 border-orange-100"
+            }`}
+          >
             {birthdaysLoading && (
               <p className="text-sm text-gray-400">Loading birthdays...</p>
             )}
@@ -113,21 +115,23 @@ export default function Dashboard() {
               <p className="text-sm text-red-400">Failed to load birthdays</p>
             )}
 
-            <div className="flex relative w-full gap-2">
-              <button
-                onClick={scrollLeft}
-                className=" absolute w-8 h-8 rounded-full bg-[#EEF2FF] flex items-center justify-center z-10 top-30"
-              >
-                <ChevronLeft size={16} />
-              </button>
+            {hasBirthdays && (
+              <div className="flex relative w-full gap-2">
+                <button
+                  onClick={scrollLeft}
+                  className="absolute w-8 h-8 rounded-full bg-[#EEF2FF] flex items-center justify-center z-10 top-30"
+                >
+                  <ChevronLeft size={16} />
+                </button>
 
-              <button
-                onClick={scrollRight}
-                className="absolute w-8 h-8 rounded-full bg-[#EEF2FF] flex items-center justify-center z-10 right-0 top-30"
-              >
-                <ChevronRight size={16} />
-              </button>
-            </div>
+                <button
+                  onClick={scrollRight}
+                  className="absolute w-8 h-8 rounded-full bg-[#EEF2FF] flex items-center justify-center z-10 right-0 top-30"
+                >
+                  <ChevronRight size={16} />
+                </button>
+              </div>
+            )}
 
             <div className="flex gap-4 overflow-hidden w-125" ref={birthdayRef}>
               {birthdays.length > 0 ? (
@@ -141,15 +145,19 @@ export default function Dashboard() {
                       image={
                         member.imageId
                           ? `https://churchsoft-backend.onrender.com/church-soft/v1.0/images/${member.imageId}`
-                          : "https://i0.wp.com/fembi.com/wp-content/uploads/2019/05/Fembi_Mortgage_join_our_team.jpg?w=1273&ssl=1"
+                          : member.gender === "MALE"
+                            ? "https://www.parentlocker.com/go/img/flat-icons/17.png"
+                            : "https://tse1.mm.bing.net/th/id/OIP.Kmc5cF6jKK_ibabYDDLyywHaHa?rs=1&pid=ImgDetMain&o=7&rm=3"
                       }
                     />
                   </div>
                 ))
               ) : (
-                <p className="text-center mt-20 text-md text-gray-500 w-full py-10">
+                <p className="text-center text-xl mt-20 text-md text-gray-400 w-full py-10">
                   No birthdays today{" "}
-                  <span className="inline-block animate-bounce text-4xl">🎉</span>
+                  <span className="inline-block animate-bounce text-4xl">
+                    🎉
+                  </span>
                 </p>
               )}
             </div>
